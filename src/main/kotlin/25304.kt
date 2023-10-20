5542692d4960c606b672ab4a4773aefdf2511b49
Your Name <psoyeon0127@naver.com>
2023-10-20 14:42:02 +0900

    week 1[fix]

--- src/main/kotlin/25304.kt
+++ src/main/kotlin/25304.kt
@@ -2,18 +2,20 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
 
-// 미해결
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val st = StringTokenizer(br.readLine())
 
     val total = br.readLine().toInt()
     val n = br.readLine().toInt()
+
     var sum = 0
 
     for (i in 1..n) {
+        val st = StringTokenizer(br.readLine())
+
         val money = st.nextToken().toInt()
         val m = st.nextToken().toInt()
+
         sum += money * m
     }
 
